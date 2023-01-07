d1393ea93a01d1bd05f915ebe4085399d4bd7e81
IlyaPrigogine <prigogine@protonmail.com>
2023-01-08 06:52:11 +0800

    Update Factory.sol

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.13;
 import "./Instance.sol";
 
 contract Factory {
+    address[] public instances;
     function create() external {
+        Instance ins = new Instance();
+        ins.initialize();
 
+        instances.push(address(ins));
     }
 }
